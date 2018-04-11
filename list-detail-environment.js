05e2eac786b72803aac90454f8c8bc65174237da
Ippei SUZUKI <ippei0605@gmail.com>
2018-04-11 18:01:20 +0900

    新規作成

---
+++ list-detail-environment.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// モジュールを読込む。
+const discovery = require('./discovery');
+
+// https://www.ibm.com/watson/developercloud/discovery/api/v1/?node#list_environment_details
+discovery.getEnvironment({environment_id: 'system'}, (error, data) => {
+    console.log(JSON.stringify(data, null, 2));
+});
--- list-environment.js
+++ list-environment.js
@@ -4,6 +4,7 @@
 const discovery = require('./discovery');
 
 // https://www.ibm.com/watson/developercloud/discovery/api/v1/?node#list_environments
-discovery.listEnvironments({}, function (error, data) {
+// WARNING: getEnvironments() was renamed to listEnvironments(). Support for getEnvironments() will be removed in the next major release
+discovery.listEnvironments({}, (error, data) => {
     console.log(JSON.stringify(data, null, 2));
 });
