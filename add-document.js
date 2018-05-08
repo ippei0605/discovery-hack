208d7fcb68be008e557c3e2069f2e031126db8da
Ippei SUZUKI <ippei0605@gmail.com>
2018-05-08 13:34:17 +0900

    作業中

--- add-document.js
+++ add-document.js
@@ -7,21 +7,17 @@ const
     discovery = require('./discovery');
 
 // https://www.ibm.com/watson/developercloud/discovery/api/v1/?node#add-doc
-fs.readFile('./data/agile.json', (error, file) => {
+
+const file = fs.createReadStream('./data/agile.json');
+discovery.addDocument({
+    environment_id: context.id,
+    collection_id: context.collection_id,
+    file: file,
+    file_content_type: 'application/json'
+}, (error, data) => {
     if (error) {
         console.log('error:', error);
     } else {
-        discovery.addDocument({
-            environment_id: context.id,
-            collection_id: context.collection_id,
-            file: file,
-            file_content_type: 'application/json'
-        }, (error, data) => {
-            if (error) {
-                console.log('error:', error);
-            } else {
-                console.log(JSON.stringify(data, null, 2));
-            }
-        });
+        console.log(JSON.stringify(data, null, 2));
     }
 });
